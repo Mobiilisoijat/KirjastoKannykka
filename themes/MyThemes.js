1cf5863655a6ffde76b2723f4e599ba5a7b617da
Martel <t2hema02@students.oamk.fi>
2025-03-18 16:43:49 +0200

    -Dark and light theme colors added -Basic folder structure added

---
+++ themes/MyThemes.js
@@ -0,0 +1,7 @@
+import { MD3DarkTheme, MD3LightTheme } from "react-native-paper";
+import { Colors } from "./constants/colors";
+
+const darkTheme = { ...MD3DarkTheme, ...Colors.dark };
+const lightTheme = { ...MD3LightTheme, ...Colors.light };
+
+export { darkTheme, lightTheme };
