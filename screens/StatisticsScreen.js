41d895d0a71da89eb116f4bdcc0dc092ad8805c3
antnyk <anttipnykanen@gmail.com>
2025-04-03 15:09:43 +0300

    added a pieChart. fixes

--- screens/StatisticsScreen.js
+++ screens/StatisticsScreen.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Button, PaperProvider, Text } from "react-native-paper";
-import { BarChart } from "react-native-gifted-charts";
+import { BarChart, PieChart } from "react-native-gifted-charts";
 import { USERS, FIREBASE_DB, FAVORITES, BOOKLIST } from '../firebase/Config';
 import { getAuth } from 'firebase/auth'
 import { query, collection, getDocs} from "firebase/firestore";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 
 function StatisticsScreen () {
   const auth = getAuth()
@@ -15,8 +15,20 @@ function StatisticsScreen () {
   const [booksReadListInYear, setBooksReadListInYear] = useState([])
   const [statsChart1, setStatsChart1] = useState([])
   const [statsChart2, setStatsChart2] = useState([])
+  const [maxHeightChart1, setMaxHeightChart1] = useState(1)
+  const [maxHeightChart2, setMaxHeightChart2] = useState(1)
   const wasListUpdated = useRef(false)
   let currentYear = 0
+  //let bookStatusList = [0, 0, 0]
+  const [bookStatusList, setBookStatusList] = useState([])
+
+  useEffect(() => {
+    // we setup max height for charts. Incase something screws up value 1 is set as the maxHeight
+    const maxHeight1 = Math.max(...statsChart1.map((obj) => obj.value), 1)
+    setMaxHeightChart1(maxHeight1)
+    const maxHeight2 = Math.max(...statsChart2.map((obj) => obj.value), 1)
+    setMaxHeightChart2(maxHeight2)
+  },[statsChart1, statsChart2])
 
   useEffect(() => {
     fetchData()
@@ -122,16 +134,26 @@ function StatisticsScreen () {
         // resetting the lists
         const tempPerYear = []
         const tempInYear = []
+        let statusList = [{value: 0, color: 'lightblue'},{value: 0, color: 'lightgreen'},{value: 0, color: 'orange'}]
         const q = query(collection(FIREBASE_DB, USERS, user.uid, BOOKLIST))
         const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
           // shows only the books we have read ('completed') per year
           if (doc.data().state == 'completed') {
+            statusList[0].value += 1
             tempPerYear.push({value: doc.data().time.slice(0,4)})
-          } if (doc.data().time && doc.data().time.startsWith(currentYear)){
-            tempInYear.push({value: doc.data().time.slice(5,7)})
+            if (doc.data().state == 'completed' && doc.data().time && doc.data().time.startsWith(currentYear)){
+              tempInYear.push({value: doc.data().time.slice(5,7)})
+            }
+          } else if (doc.data().state == 'planning'){
+            statusList[1].value += 1
+          } else if (doc.data().state == 'reading'){
+            statusList[2].value += 1
           }
         })
+        setBookStatusList([...statusList])
+        bookStatusList.forEach(i => console.log('b', i))
+        statusList.forEach(i => console.log('b', i))
         // if tempYear is not empty we set wasListUpdated to true. This in turn makes sure that the chart is shown
         if (tempInYear.length > 0){
           wasListUpdated.current = true
@@ -146,6 +168,23 @@ function StatisticsScreen () {
     }
   }
 
+  const renderLegend = (text, color) => {
+    return (
+      <View style={{flexDirection: 'row', marginBottom: 12}}>
+        <View
+          style={{
+            height: 18,
+            width: 18,
+            marginRight: 10,
+            borderRadius: 4,
+            backgroundColor: color || 'white',
+          }}
+        />
+        <Text style={{color: 'black', fontSize: 16}}>{text || ''}</Text>
+      </View>
+    );
+  };
+
   return(
     <PaperProvider>
       <ScrollView style={{marginBottom: 60}}>
@@ -158,23 +197,62 @@ function StatisticsScreen () {
             marginTop: 16
           }}
           >
+          Kirjalistan tiedot
+        </Text>
+        {
+          (statsChart1.length > 0)
+          ?
+          <View>
+            <PieChart
+              showValuesAsLabels
+              showText
+              textColor="black"
+              radius={150}
+              textSize={20}
+              showTextBackground
+              textBackgroundRadius={26}
+              data={bookStatusList}
+              onPress={() => bookStatusList.forEach(i => console.log(i))}
+            />
+            <View
+              style={{
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                marginTop: 20,
+              }}>
+              {renderLegend('Luettu', 'lightblue')}
+              {renderLegend('Aikomus lukea', 'lightgreen')}
+              {renderLegend('Kesken', 'orange')}
+            </View>
+          </View>
+          :
+          <Text>Lisää kirja luetuksi lukulistalle nähdäksesi tulokset!</Text>
+        }
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            marginTop: 16
+          }}
+          >
         Luetut kirjat per vuosi
         </Text>
         {
-          // bug: if only one book in the list the chart shows up funny. is related to noOfSections and maxValue
           (statsChart1.length > 0)
           ?
           <BarChart
             barWidth={24}
-            //noOfSections={statsChart1.length > 1 ? undefined : 1} // undefined uses the default value
+            noOfSections={maxHeightChart1 > 2 ? 3 : 1}
             barBorderRadius={2}
             disablePress={true}
             frontColor="blue"
             data={statsChart1}
             roundToDigits={0}
             yAxisThickness={1}
-            xAxisThickness={0}
-            //maxValue={statsChart1.length < 2 && 1}
+            xAxisThickness={1}
+            maxValue={maxHeightChart1}
             isAnimated
           />
           :
@@ -194,27 +272,44 @@ function StatisticsScreen () {
           (wasListUpdated.current == true)
           ?
           <BarChart
-          barWidth={24}
-          //noOfSections={statsChart2.length > 1 ? undefined : 1} // undefined uses the default value
-          barBorderRadius={2}
-          disablePress={true}
-          frontColor="red"
-          data={statsChart2}
-          roundToDigits={0}
-          yAxisThickness={1}
-          xAxisThickness={0}
-          //maxValue={statsChart2.length < 2 && 1}
-          isAnimated
+            barWidth={24}
+            noOfSections={maxHeightChart2 > 2 ? 3 : 1}
+            barBorderRadius={2}
+            disablePress={true}
+            frontColor="red"
+            data={statsChart2}
+            roundToDigits={0}
+            yAxisThickness={1}
+            xAxisThickness={1}
+            maxValue={maxHeightChart2}
+            isAnimated
           />
           :
           <Text>Et ole lukenut tänä vuonna kirjoja!</Text>
         }
+        {
+          /* SAVED FOR TESTING!
+          <Button
+            onPress={() => {
+              const maxHeight = Math.max(...statsChart1.map((obj) => obj.value))
+              setMaxHeightChart1(maxHeight)
 
-        <Button
-          onPress={() => firebaseGetBookList()}
-          >
-          BookList
-        </Button>
+              const maxHeight2 = Math.max(...statsChart2.map((obj) => obj.value))
+              setMaxHeightChart2(maxHeight2)
+              //statsChart1.forEach((item) => {
+              //  console.log(item.value)
+              //})
+            }}
+            >
+            test
+          </Button>
+          <Button
+            onPress={() => firebaseGetBookList()}
+            >
+            BookList
+          </Button>
+          */
+        }
 
       </ScrollView>
     </PaperProvider>
