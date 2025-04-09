962d17d61f5f3bbadeff5643959b06d8664fa9ef
antnyk <anttipnykanen@gmail.com>
2025-04-09 14:30:44 +0300

    portOpener.py file - not needed. Chatbot authentication for API calls

--- screens/ChatbotScreen.js
+++ screens/ChatbotScreen.js
@@ -1,7 +1,7 @@
 import react, { useState } from "react";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { Button, Text, PaperProvider, TextInput } from "react-native-paper";
-import { LLM_API_URL } from "../firebase/Config";
+import { LLM_API_URL, LLM_PASSWORD, LLM_USER } from "../firebase/Config";
 
 function ChatbotScreen() {
   const [text, setText] = useState("Hello")
@@ -15,15 +15,16 @@ function ChatbotScreen() {
       const res = await fetch(url, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          "Authorization": `Basic ${btoa(LLM_USER + ":" + LLM_PASSWORD)}`,
         },
         body: JSON.stringify({
           // for Kobold
           "prompt": `${text}`,
           "max_tokens": 50,
           "temperature": 0.6,
-          "min_P": 0.1, // not necessarely needed
-          "top_K": 10, // not necessarely needed
+          "top_K": 10, // not necessarely needed - chooses the most likely word to use from pool of 10 words related to the topic
+          //"min_P": 0.1, // not necessarely needed
           // for Ollama
           //"options": {
           //  "model": `${LLM_MODEL_NAME}`,
@@ -32,7 +33,7 @@ function ChatbotScreen() {
         })
       })
       if (!res.ok) {
-        console.log("an error accured")
+        console.log("an error accured:", res)
         return
       }
       const json = await res.json()
@@ -45,20 +46,29 @@ function ChatbotScreen() {
 
   return(
     <PaperProvider>
-      <View>
-        <Button
-          mode="contained"
-          onPress={() => fetchFunction(text)}
-          >
-          Get
-        </Button>
-        <TextInput
-          label={'Write'}
-          value={text}
-          onChangeText={text => setText(text)}
-          >
-        </TextInput>
-        <Text>Response: {responseText}</Text>
+      <View style={{flex: 1}}>
+        <View style={{}}>
+          <Text>KirjaBotti:</Text>
+          <ScrollView contentContainerStyle={{paddingBottom: 210}}>
+            <Text>{responseText}</Text>
+          </ScrollView>
+        </View>
+        <View style={{position: 'absolute', bottom: 50, left: 0, right: 0}}>
+          <Button
+            mode="contained"
+            style={{width: 140}}
+            onPress={() => fetchFunction(text)}
+            >
+            Send
+          </Button>
+          <TextInput
+            label={'Write'}
+            multiline={true}
+            value={text}
+            onChangeText={text => setText(text)}
+            >
+          </TextInput>
+        </View>
       </View>
     </PaperProvider>
   )
